c5004ea9f70cbd75884905a590698cd3a90424fe
jarungii <tannokrr@gmail.com>
2024-11-29 19:39:03 +0400

    pause screen update: new background, new resume button, new menu button

--- app/src/main/java/com/conductorsimulator/gamescreens/PauseScreen.kt
+++ app/src/main/java/com/conductorsimulator/gamescreens/PauseScreen.kt
@@ -1,20 +1,22 @@
 package com.conductorsimulator.gamescreens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.offset
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import com.conductorsimulator.R
 import com.conductorsimulator.gamelogic.GameEvent
 import com.conductorsimulator.gamelogic.GameState
 
@@ -30,28 +32,48 @@ fun PauseScreen(
     navController: NavController,
     onEvent: (GameEvent) -> Unit
     ) {
+    Image(
+        painter = painterResource(id = R.drawable.pause_bg),
+        contentDescription = "pause_bg",
+        contentScale = ContentScale.FillHeight,
+    )
     Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
         modifier = Modifier
+            .offset(y =145.dp)
             .fillMaxSize()
-            .padding(10.dp)
     ) {
-        Text(
-            text = "PAUSE",
-            color = Color.Cyan,
-            fontSize = 30.sp,
-            textAlign = TextAlign.Center,
-            fontWeight = FontWeight.Bold
+        Image(
+            painter = painterResource(id = R.drawable.resumebutton),
+            contentDescription = "resume_button",
+            alignment = Alignment.Center,
+            modifier = Modifier
+                .clickable(onClick = {
+                    onEvent(GameEvent.TerminalOn)
+                    navController.navigate("play")
+                })
+                .scale(0.9f)
         )
-        Button(
-            onClick = {
-                navController.navigate("menu")
-                onEvent(GameEvent.KillGame)
-            }
-        ) {
-            Text(
-                text = "menu"
-            )
-        }
+    }
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .offset(y =290.dp)
+            .fillMaxSize()
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.menubutton),
+            contentDescription = "menubutton",
+            alignment = Alignment.Center,
+            modifier = Modifier
+                .clickable(onClick = {
+                    onEvent(GameEvent.KillGame)
+                    navController.navigate("menu")
+                })
+                .scale(0.9f)
+        )
     }
 
 }
